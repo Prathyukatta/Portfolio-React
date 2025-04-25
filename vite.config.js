e36fb736be11cf58aedecd989d2420faeed2c090
Prathyukatta <prathyushakatta1@gmail.com>
2025-04-25 13:49:36 +0530

    Clean initial commit

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vite.dev/config/
 export default defineConfig({
-  base: "/Portfolio-React/",
+  base: "/Portfolio-React",
   plugins: [react()],
 });
